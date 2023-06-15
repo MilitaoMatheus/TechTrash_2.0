539778e129d5475c956450216bcb2e8c755c05a2
Matheus Militão <matheus.silva2533@etec.sp.gov.br>
2023-06-15 12:14:31 -0300

    Criação da segunda tela com as informações necessárias.

--- app/src/main/java/com/example/techtrash/TelaInfo1.java
+++ app/src/main/java/com/example/techtrash/TelaInfo1.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class TelaInfo1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        /*Declarando as variáveis*/
+        setContentView(R.layout.infos1);
     }
 }
