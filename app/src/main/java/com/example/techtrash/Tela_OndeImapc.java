6d88fe5cb33009d7a0a8880efaa8d2d226ace996
Matheus Militão <matheus.silva2533@etec.sp.gov.br>
2023-06-16 12:04:01 -0300

    Já criamos as outras telas de informação (3), mas sem nenhum item de "Consulta".

---
+++ app/src/main/java/com/example/techtrash/Tela_OndeImapc.java
@@ -0,0 +1,14 @@
+package com.example.techtrash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tela_OndeImapc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_onde_imapc);
+    }
+}
--- app/src/main/java/com/example/techtrash/tela_oque.java
+++ app/src/main/java/com/example/techtrash/tela_oque.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TelaInfo1 extends AppCompatActivity {
+public class tela_oque extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.infos1);
+        setContentView(R.layout.activity_tela_oque);
     }
 }
