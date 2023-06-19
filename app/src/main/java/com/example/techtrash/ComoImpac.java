34d9de1cfe8803ebed586aded8205eca143e2f5f
Matheus Militão da Silva <matheus.silva2533@etec.sp.gov.br>
2023-06-19 18:34:44 -0300

    O layout de todas as telas já estão formados, agora falta somente as informações do tema do aplicativo e a parte da programação.

---
+++ app/src/main/java/com/example/techtrash/ComoImpac.java
@@ -0,0 +1,14 @@
+package com.example.techtrash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComoImpac extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_como_impac);
+    }
+}
---
+++ app/src/main/java/com/example/techtrash/DescarteReutilizacao.java
@@ -0,0 +1,14 @@
+package com.example.techtrash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DescarteReutilizacao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_descarte_reutilizacao);
+    }
+}
